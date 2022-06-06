3587742f940fc5cf4937c481cb5761433d41429b
lowie1803 <dung.totuan01@gmail.com>
2022-06-06 23:47:09 +0700

    template for codeforces polygon problem package

---
+++ templates/new-polygon-problems/validatow.cpp
@@ -0,0 +1,13 @@
+#include "testlib.h"
+
+const int MAXT = 50'000;
+const int MAXN = 100'000;
+const int MAXA = 1'000'000'000;
+
+// todo: copy doc from: https://codeforces.com/blog/entry/18426 thanks RR
+
+int main(int argc, char* argv[]) {
+  registerValidation(argc, argv);
+  int testCount = inf.readInt(1, MAXT, "testCount");
+  inf.readEoln();
+}
